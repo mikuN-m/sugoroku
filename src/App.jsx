e6073de936c6cc0abeb1c02692daec7000706545
miku <nunon4msmrn@icloud.com>
2023-07-26 13:46:42 +0900

    🎉 環境構築

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+class App extends React.Component {
+  render(){
+    return(
+      <div>
+        <div className="test">
+          <h1>hello</h1>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
