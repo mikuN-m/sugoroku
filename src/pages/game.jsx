4a069ec0cc292f6426a7766d2ff506ed453638af
miku <nunon4msmrn@icloud.com>
2023-08-22 12:51:47 +0900

    docs ✨ ターンを進める

--- src/pages/game.jsx
+++ src/pages/game.jsx
@@ -9,7 +9,8 @@ class Game extends React.Component {
             color: [],
             x: 0,
             y: 0,
-            space: [0,0,0,0]
+            space: [0,0,0,0],
+            turn: 0
         }
     }
 
@@ -48,7 +49,7 @@ class Game extends React.Component {
 
             setTimeout(()=>{
                 step1();
-            },1000)
+            },500)
         };
         
         for(let i=0; i<this.state.name.length; i++){
@@ -62,11 +63,11 @@ class Game extends React.Component {
             document.getElementsByClassName('player')[i].style.display = 'block';
         }            
 
-
-        let turn = 0;
         const step1 = () => {
-            window.alert(`${this.state.name[turn]}さんのターンです\nダイスを振ってください`);
-            document.getElementsByClassName('dice-btn')[0].style.display = 'block';
+            window.alert(`${this.state.name[this.state.turn]}さんのターンです\nダイスを振ってください`);
+            document.getElementById('next-turn-btn').style.display = 'none';
+            document.getElementById('turn-btn').style.display = 'none';
+            document.getElementById('dice-btn').style.display = 'block';
         };
 
 
@@ -81,19 +82,39 @@ class Game extends React.Component {
             dice = diceNam + 1;
 
             document.getElementById('dice-btn').style.display = 'none';
-            document.getElementById('next-turn-btn').style.display = 'block';
+            document.getElementById('turn-btn').style.display = 'block';
         }  
 
 
-        const spaceBox = document.getElementsByClassName('space');
         const step3 = () => {
-            const newVal = newSpace[turn] + dice;
-            // newSpace[turn] = newVal;
-            // this.setState({
-            //     space: newSpace
-            // },()=>{
-                window.alert(newVal)
-            // });
+            const newVal = newSpace[0][this.state.turn] + dice;
+            newSpace[0][this.state.turn] = newVal;
+            this.setState({
+                space: newSpace[0]
+            },()=>{
+                const nowSpace = document.getElementById(`space${this.state.space[this.state.turn]}`);
+
+                nowSpace.style.backgroundColor = this.state.color[this.state.turn];
+            });
+
+            document.getElementById('turn-btn').style.display = 'none';
+            document.getElementById('next-turn-btn').style.display = 'block';
+        };
+
+        const step4 = () => {
+            if (this.state.turn < this.state.name.length-1) {
+                this.setState({
+                    turn: this.state.turn + 1
+                },()=>{
+                    step1();
+                });
+            } else {
+                this.setState({
+                    turn: 0
+                },()=>{
+                    step1();
+                });
+            }
         };
 
 
@@ -140,8 +161,9 @@ class Game extends React.Component {
 
                             <div className="dice-content">
 
-                                <button className="btn dice-btn" id="dice-btn" onClick={()=>{step2()}}>ダイスを振る</button>
-                                <button className="btn dice-btn" id="next-turn-btn" onClick={()=>{step3()}}>ターンを進める</button>
+                                <button className="btn dice-btns" id="dice-btn" onClick={()=>{step2()}}>ダイスを振る</button>
+                                <button className="btn dice-btns" id="turn-btn" onClick={()=>{step3()}}>ターンを進める</button>
+                                <button className="btn dice-btns" id="next-turn-btn" onClick={()=>{step4()}}>ターン終了</button>
 
                                 <div className="dice-img">
                                     
@@ -185,35 +207,51 @@ class Game extends React.Component {
                                         </div>
 
                                     </div>
-                                    <div className="space"></div>
-                                    <div className="space"></div>
-                                    <div className="space"></div>
-                                    <div className="space"></div>
+                                    <div className="space" id="space1"></div>
+                                    <div className="space" id="space2"></div>
+                                    <div className="space" id="space3"></div>
+                                    <div className="space" id="space4"></div>
                                 </div>
 
                                 <div className="space-box right">
-                                    <div className="space"></div>
+                                    <div className="space" id="space5"></div>
                                 </div> 
 
                                 <div className="space-box">
-                                    <div className="space"></div>
-                                    <div className="space"></div>
-                                    <div className="space"></div>
-                                    <div className="space"></div>
-                                    <div className="space"></div>
+                                    <div className="space" id="space10"></div>
+                                    <div className="space" id="space9"></div>
+                                    <div className="space" id="space8"></div>
+                                    <div className="space" id="space7"></div>
+                                    <div className="space" id="space6"></div>
                                 </div>
 
                                 <div className="space-box left">
-                                    <div className="space"></div>
+                                    <div className="space" id="space11"></div>
                                 </div> 
 
                                 <div className="space-box">
-                                    <div className="space"></div>
-                                    <div className="space"></div>
-                                    <div className="space"></div>
-                                    <div className="space"></div>
-                                    <div className="space"></div>
+                                    <div className="space" id="space12"></div>
+                                    <div className="space" id="space13"></div>
+                                    <div className="space" id="space14"></div>
+                                    <div className="space" id="space15"></div>
+                                    <div className="space" id="space16"></div>
                                 </div>
+
+                                <div className="space-box right">
+                                    <div className="space" id="space17"></div>
+                                </div> 
+
+                                <div className="space-box">
+                                    <div className="space" id="space21"></div>
+                                    <div className="space" id="space20"></div>
+                                    <div className="space" id="space19"></div>
+                                    <div className="space" id="space18"></div>
+                                    <div className="space" id="space17"></div>
+                                </div>
+
+                                <div className="space-box left">
+                                    <div className="space" id="space22">ゴール</div>
+                                </div> 
 
                             </div>                            
 
